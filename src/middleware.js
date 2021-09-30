ee5579e79a0e24cc013808e7863b134424e4a265
Edgaras Stepanovas <78696812+Edgaras98@users.noreply.github.com>
2021-09-30 21:48:28 +0300

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -6,7 +6,7 @@ const { jswtSecret } = require('./config');
 module.exports = {
   isLoggedin: (req, res, next) => {
     try {
-      const token = req.headers.authorization?.split(' ')[1];
+      const token = req.headers.authorization?.split(' ')[1]: '';
       const decodedToken = jwt.verify(token, jswtSecret);
       req.userData = decodedToken;
       return next();
